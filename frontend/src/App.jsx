5d567a355d29dc0771efc04ed1386d8cc75e764c
sandroservo <sandro_servo@hotmail.com>
2023-07-16 21:34:13 -0300

    foi adicionado o  reatc-redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-
+import 
 
 export function App() {
   
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,3 +3,15 @@ import { persistStore } from "redux-persist"
 
 import persistReducer from "./persisteReducer"
 import createStore from "./createStore"
+import rootReducer from "./modules/rootReducer"
+import rootSaga from "./modules/rootSaga"
+
+const sagamMiddleware = CreateSagaMiddleware({ sagamMonitor : null})
+const middlewares =  [sagamMiddleware]
+
+const store =  createStore(persistReducer(rootReducer), middlewares);
+const persistor =  persistStore(store);
+
+sagamMiddleware.run(rootSaga);
+
+export {store, persistor}
