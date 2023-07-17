d7731cc087e83468dda6bbfaf3bd6d30a2be8267
sandroservo <sandro_servo@hotmail.com>
2023-07-16 21:43:03 -0300

    redux configurado com sucesso

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import 
+import { Provider  } from "react-redux"
+import  {PersistGate } from "redux-persist/integration/react"
+import { store, persistor } from "./store"
 
 export function App() {
-  
+  return (
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <h1>Welcome System</h1>
+        </PersistGate>
+
+    </Provider>
+  )
 }
 
-export default App
+
